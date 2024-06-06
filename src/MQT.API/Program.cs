3d3386095f6971271d96c9f083c750ecac7ab04e
d3bes <abdo.mohamed6319@gmail.com>
2024-06-06 16:22:38 +0300

    basic modeles

---
+++ src/MQT.API/Program.cs
@@ -0,0 +1,50 @@
+using System.Net.NetworkInformation;
+using System.Buffers;
+using System.Net.Http.Headers;
+using Microsoft.EntityFrameworkCore;
+using MQT.API.Models;
+using MQT.EF;
+using Microsoft.Extensions.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MqtDbContext>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    
+ContactInformation contact = new ContactInformation();
+contact.LinkedInProfile = "jgjoj";
+Console.WriteLine(contact);
+    return contact;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
